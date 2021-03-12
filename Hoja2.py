0ed6866383315f7c9fbfdbbf96306400bde9fcb9
Diego Barrios <69433981+htmlfinalproject@users.noreply.github.com>
2021-03-12 15:44:16 -0600

    Add files via upload

---
+++ Hoja2.py
@@ -0,0 +1,25 @@
+#ejercicio 1
+
+key = "contraseña"
+password = input("Introduce la contraseña: ")
+if key == password.lower():
+    print("La contaseña coincide")
+else:
+    print("La contraseña no coincide")
+
+
+#ejercicio 2
+
+name = input("¿Cómo te llamas? ")
+gender = input("¿Cuál es tu sexo (M o H)? ")
+if gender == "M":
+    if name.lower() < "m":
+        group = "A"
+    else:
+        group = "B"
+else:
+    if name.lower() > "n":
+        group = "A"
+    else:
+        group = "B"
+print("Tu grupo es " + group)
